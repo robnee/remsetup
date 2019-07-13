bf7a742dfa113f84329c64c1a0ef7f809f93d701
robnee <robert.nee@gmail.com>
2019-07-12 21:39:36 -0400

    improve locale handling

--- config.sh
+++ config.sh
@@ -8,61 +8,203 @@
 
 #-------------------------------------------------------------------------------
 
-do_timezone()
+do_packages()
 {
 	echo
-	echo "Set /etc/timezone to $1 ..."
+	echo "package install update and install ..."
 
-	echo $1 | sudo cat > /etc/timezone
+	sudo apt --yes update --allow-releaseinfo-change
+	sudo apt --yes update
+	sudo apt --yes upgrade
+
+	sudo apt --yes install git subversion lirc lirc-doc
+}
+
+#-------------------------------------------------------------------------------
+
+do_timezone()
+{
+	local file=/etc/timezone
+
+	grep --quiet "$1" $file
+	if [ "$?" -ne "0" ]; then
+		echo "Set $file to $1 ..."
+
+		sudo timedatectl set-timezone $1
+
+		config_count=$(( $config_count + 1 ))
+	else
+		echo "$file already set to $1"
+	fi
 }
 
 #-------------------------------------------------------------------------------
 
 do_hostname()
 {
-	echo
-	echo "Set /etc/hostname to $1 ..."
+	local file=/etc/hostname
 
-	echo $1 | sudo cat > /etc/hostname
+	grep --quiet "$1" $file
+	if [ "$?" -ne "0" ]; then
+		echo "Set $file to $1 ..."
 
-	sed s/raspberrypi/$1/ /etc/hosts | sudo cat > /etc/hosts
+		echo $1 | sudo dd status=none of=$file
+
+		sed s/raspberrypi/$1/ /etc/hosts | sudo dd status=none of=/etc/hosts
+
+		config_count=$(( $config_count + 1 ))
+	else
+		echo "$file already set to $1"
+	fi
 }
 
 #-------------------------------------------------------------------------------
 
-do_packages()
+do_locale()
 {
-	echo
-	echo "package install update and install ..."
+	local file=/etc/default/locale
 
-	sudo apt-get --yes update
-	sudo apt-get --yes upgrade
+	grep --quiet "$1" $file
+	if [ "$?" -ne "0" ]; then
+		echo "Set $file to $1 ..."
 
-	sudo apt-get --yes install git
-	sudo apt-get --yes install subversion
-	sudo apt-get --yes install lirc
+		echo LANG=$1 | sudo dd status=none of=$file
+
+		config_count=$(( $config_count + 1 ))
+	else
+		echo "$file already set to $1"
+	fi
+}
+
+#-------------------------------------------------------------------------------
+
+do_keyboard()
+{
+	local file=/etc/default/keyboard
+
+	grep --quiet "$1" $file
+	if [ "$?" -ne "0" ]; then
+		echo "set $file to $1 $2 ..."
+
+		sudo cp -f $file $file.orig
+		cat <<-EOF | sudo dd status=none of=$file
+		    # IR Remote Settings
+		    XKBMODEL="$1"
+		    XKBLAYOUT="$2"
+		    XKBVARIANT=""
+		    XKBOPTIONS=""
+
+		    BACKSPACE="guess"
+		EOF
+
+		config_count=$(( $config_count + 1 ))
+	else
+		echo "$file already set to $1 $2"
+	fi
 }
 
 #-------------------------------------------------------------------------------
 
 do_boot_config()
 {
-	echo
-	echo "update /boot/config.txt ..."
+	local file=/boot/config.txt
 
-	grep --quiet "gpu_mem=16" /boot/config.txt
+	grep --quiet "gpu_mem=16" $file
 	if [ "$?" -ne "0" ]; then
-		sudo cp -f /boot/config.txt /boot/config.txt.orig
-		cat <<-EOF | sudo tee --append /boot/config.txt
+		echo "update $file gpio_pin $1 ..."
+
+		sudo cp -f $file $file.orig
+		cat <<-EOF | sudo tee --append $file
 
 			# IR Remote Settings
 			gpu_mem=16
-			dtoverlay=gpio-ir-tx,gpio_pin=22
+			dtoverlay=gpio-ir-tx,gpio_pin=$1
 		EOF
 
 		config_count=$(( $config_count + 1 ))
 	else
-		echo "/boot/config.txt already configured"
+		echo "$file already configured"
+	fi
+}
+
+#-------------------------------------------------------------------------------
+
+do_lirc_options()
+{
+	local file=/etc/lirc/lirc_options.conf
+
+	grep --quiet "lirc0" $file
+	if [ "$?" -ne "0" ]; then
+		echo "create $file port $1 ..."
+
+		if [ -e $file ]; then
+			sudo cp -vf $file $file.orig
+		fi
+
+		cat <<-EOF | sudo tee $file
+			# IR Remote Settings
+
+			[lircd]
+			nodaemon        = False
+			driver          = default
+			device          = /dev/lirc0
+			output          = /var/run/lirc/lircd
+			pidfile         = /var/run/lirc/lircd.pid
+			plugindir       = /usr/lib/arm-linux-gnueabihf/lirc/plugins
+			permission      = 666
+			allow-simulate  = No
+			repeat-max      = 600
+			#effective-user =
+			listen          = $1
+			#connect        = host[:port]
+			logfile         = /var/log/lirc.log
+			loglevel        = 6
+			#release        = true
+			#release_suffix = _EVUP
+			#driver-options = ...
+
+			[lircmd]
+			uinput          = False
+			nodaemon        = False
+
+			# [modinit]
+			# code = /usr/sbin/modprobe lirc_serial
+			# code1 = /usr/bin/setfacl -m g:lirc:rw /dev/uinput
+			# code2 = ...
+
+
+			# [lircd-uinput]
+			# add-release-events = False
+			# release-timeout    = 200
+			# release-suffix     = _EVUP
+		EOF
+
+		config_count=$(( $config_count + 1 ))
+	else
+		echo "$file already configured"
+	fi
+}
+
+#-------------------------------------------------------------------------------
+
+do_lircd_conf()
+{
+	local file=/etc/lirc/lircd.conf
+
+	if [ ! -e $file ]; then
+		echo "create $file ..."
+
+		cat <<-EOF | sudo tee $file
+			# IR Remote Settings
+
+			#include "TV.conf"
+			#include "CABLE.conf"
+			#include "RECEIVER.conf"
+		EOF
+
+		config_count=$(( $config_count + 1 ))
+	else
+		echo "$file already configured"
 	fi
 }
 
@@ -78,7 +220,7 @@ do_etc_modules()
 	if [ "$?" -ne "0" ]; then
 		sudo cp -vf /etc/modules /etc/modules.orig
 		cat <<-EOF | sudo tee --append /etc/modules
-		
+
 			# IR Remote Settings
 			lirc_dev
 			lirc_rpi gpio_in_pin=23 gpio_out_pin=22
@@ -110,24 +252,24 @@ do_lirc_hardware()
 			# Arguments which will be used when launching lircd
 			#LIRCD_ARGS="--uinput"
 			LIRCD_ARGS="--listen"
-			
+
 			# Don't start lircmd even if there seems to be a good config file
 			# START_LIRCMD=false
 			q
-			
+
 			# Don't start irexec, even if a good config file seems to exist.
 			# START_IREXEC=false
-			
+
 			# Try to load appropriate kernel modules
 			LOAD_MODULES=true
-			
+
 			# Run "lircd --driver=help" for a list of supported drivers.
 			# todo: what does this option do?
 			DRIVER="default"
 			# usually /dev/lirc0 is the correct setting for systems using udev
 			DEVICE="/dev/lirc0"
 			MODULES="lirc_rpi"
-			
+
 			# Default configuration files for your hardware if any
 			LIRCD_CONF=""
 			LIRCMD_CONF=""
@@ -141,63 +283,6 @@ do_lirc_hardware()
 
 #-------------------------------------------------------------------------------
 
-do_lirc_options()
-{
-	echo
-	echo "create /etc/lirc/lirc_options.conf ..."
-
-	grep --quiet "lirc0" /etc/lirc/lirc_options.conf
-	if [ "$?" -ne "0" ]; then
-		if [ -e /etc/lirc/lirc_options.conf ]; then
-			sudo cp -vf /etc/lirc/lirc_options.conf /etc/lirc/lirc_options.conf.orig
-		fi
-
-		cat <<-EOF | sudo tee /etc/lirc/lirc_options.conf
-			# IR Remote Settings
-
-			[lircd]
-			nodaemon        = False
-			driver          = default
-			device          = /dev/lirc0
-			output          = /var/run/lirc/lircd
-			pidfile         = /var/run/lirc/lircd.pid
-			plugindir       = /usr/lib/arm-linux-gnueabihf/lirc/plugins
-			permission      = 666
-			allow-simulate  = No
-			repeat-max      = 600
-			#effective-user =
-			listen          = 8765
-			#connect        = host[:port]
-			logfile         = /var/log/lirc.log
-			loglevel        = 6
-			#release        = true
-			#release_suffix = _EVUP
-			#driver-options = ...
-
-			[lircmd]
-			uinput          = False
-			nodaemon        = False
-
-			# [modinit]
-			# code = /usr/sbin/modprobe lirc_serial
-			# code1 = /usr/bin/setfacl -m g:lirc:rw /dev/uinput
-			# code2 = ...
-
-
-			# [lircd-uinput]
-			# add-release-events = False
-			# release-timeout    = 200
-			# release-suffix     = _EVUP
-		EOF
-
-		config_count=$(( $config_count + 1 ))
-	else
-		echo "/etc/lirc/lirc_options.conf already configured"
-	fi
-}
-
-#-------------------------------------------------------------------------------
-
 # Get the date info
 day=`date +%A`
 dom=`date +%d`
@@ -206,14 +291,13 @@ mon=`date +%B`
 ymd=`date +%Y-%m-%d`
 start_time=`date +'%Y-%m-%d %H:%M:%S'`
 
-MNTDIR=./mnt
-LOGDIR=./logs
-LOGFILE=$LOGDIR/$ymd.log
 SCRIPTDIR=$(dirname $0)
 HOSTNAME=remstage
+LIRCPORT=8765
+LIRCPIN=23
 UNAME=$(uname -a)
 
-config_count=0
+. /etc/os-release
 
 echo
 echo --------------------------------------------------------------------------------
@@ -222,36 +306,22 @@ echo ---------------------------------------------------------------------------
 
 echo "Hostname      : $HOSTNAME"
 echo "System        : $UNAME"
-echo
+echo "Release       : $NAME $VERSION"
 
-do_timezone "US/Eastern"
+config_count=0
+
 do_hostname $HOSTNAME
+do_timezone "America/New_York"
+do_locale "C.UTF-8"
+do_keyboard "pc101" "us"
 
 do_packages
-do_boot_config
-#do_etc_modules
-#do_lirc_hardware
-do_lirc_options
+do_boot_config $LIRCPIN
+do_lirc_options $LIRCPORT
+do_lircd_conf
 
 echo
-echo "create /etc/lirc/lircd.conf ..."
-
-if [ ! -e /etc/lirc/lircd.conf ]; then
-	cat <<-EOF | sudo tee /etc/lirc/lircd.conf
-		# IR Remote Settings
-
-		#include "TV.conf"
-		#include "CABLE.conf"
-		#include "RECEIVER.conf"
-	EOF
-
-	config_count=$(( $config_count + 1 ))
-else
-	echo "/etc/lirc/lircd.conf already configured"
-fi
-
-echo
-echo "enable Lirc service ..."
+echo "made $config_count config file changes"
 
 # See if we should reboot
 if [ $config_count -gt 0 ]; then
@@ -261,6 +331,10 @@ if [ $config_count -gt 0 ]; then
 fi
 
 # After first reboot
+`
+echo
+echo "enable Lirc service ..."
+
 sudo systemctl enable lircd
 sudo systemctl start lircd
 sudo systemctl status lircd
